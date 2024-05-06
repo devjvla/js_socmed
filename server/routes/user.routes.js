556aee0e7976069c6aaebcac9a4f57b91a37393f
devjvla <dev.jvla@gmail.com>
2024-05-06 15:12:55 +0800

    Add sign in feature

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,5 +5,6 @@ import UsersController from "../controllers/users.controller.js";
 const UserRouter = Router();
 
 UserRouter.post("/signup", ValidationHelper.SignupForm, UsersController.signup);
+UserRouter.post("/signin", ValidationHelper.SigninForm, UsersController.signin);
 
 export default UserRouter;
